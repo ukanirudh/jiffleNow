7476baceeca7eb8d987f7ba5cfd7950c1989af66
U K ANIRUDH <U K ANIRUDH>
2017-04-16 12:21:04 +0530

    Changing http request to pull data from URL rather than local json file

--- app/public/js/appFactory.js
+++ app/public/js/appFactory.js
@@ -1,7 +1,8 @@
 angular.module('myApp').factory('appFactory',function($http){
 		var data;
 		function getData() {
-			data = $http.get('data/questions.json');
+			data = $http.get('https://cdn.rawgit.com/santosh-suresh/39e58e451d724574f3cb/raw/784d83b460d6c0150e338c34713f3a1c2371e20a/assignment.json');
+			//'data/questions.json'(local json file..)
 			return data;
 		}
 
